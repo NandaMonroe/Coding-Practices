7bc5b6cf502b5db0e30ebf1f22c041e444d628ca
Fernanda Monroe <inandamonroe@gmail.com>
2023-08-27 12:15:33 -0300

    Adjusting the function to converto from F > C and from C > F

--- Dojo_Weather/script.js
+++ Dojo_Weather/script.js
@@ -3,11 +3,23 @@ function removeCookie(id) {
     elem.remove()
 }
 
-function fahrenheit(id) {
-    var elem = document.querySelector(id)
-
-    elem.innerText = Math.round((elem.innerText * (9/5)) + 32);
-    
+function c2f(temp) {
+    return Math.round(9 / 5 * temp + 32);
 }
 
-``
+function f2c(temp) {
+    return Math.round(5 / 9 * (temp - 32));
+}
+
+function convert(element) {
+    console.log(element.value);
+    for(var i=1; i<9; i++) {
+        var tempSpan = document.querySelector("#temp" + i);
+        var tempVal = parseInt(tempSpan.innerText);
+        if(element.value == "°C") {
+            tempSpan.innerText = f2c(tempVal);
+        } else {
+            tempSpan.innerText = c2f(tempVal);
+        }
+    }
+}
